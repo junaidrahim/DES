0c234cae6c94beef395e4b4899d736521ad6f203
Junaid H Rahim <junaidrahim8d@gmail.com>
2021-10-25 23:09:19 +0530

    bug fixes

--- main.cpp
+++ main.cpp
@@ -12,44 +12,66 @@
 #include "src/DES.h"
 #include "src/argh.h"
 
-std::vector<std::string> cipher_decipher(const std::string& key, const std::string& plaintext) {
+std::vector<std::string> encrypt(const std::string& key, const std::string& plain_text) {
     auto keys = RoundKey::generate_keys(key);
-
     auto des = DES(keys);
-    std::string cipher_text = des.run(plaintext);
-
-    des.reverse_keys();
-    std::string decrypted_cipher_text = des.run(cipher_text);
+    std::string encrypted_cipher_text = des.run(plain_text);
 
     std::vector<std::string> encrypted_bytes;
 
-    for(int i = 0; i < plaintext.size(); i += 8) {
-        std::cout << cipher_text.substr(i, 8) << std::endl;
-        encrypted_bytes.push_back(cipher_text.substr(i, 8));
+    for(int i = 0; i < plain_text.size(); i += 8) {
+        std::cout << encrypted_cipher_text.substr(i, 8) << std::endl;
+        encrypted_bytes.push_back(encrypted_cipher_text.substr(i, 8));
     }
-
-    std::cout << std::endl;
 
     return encrypted_bytes;
 }
 
-int main(int argc, char** argv) {
+std::vector<std::string> decrypt(const std::string& key, const std::string& encrypted_text) {
+    auto keys = RoundKey::generate_keys(key);
+    auto des = DES(keys);
+    des.reverse_keys();
+    std::string decrypted_cipher_text = des.run(encrypted_text);
 
+    std::vector<std::string> decrypted_bytes;
+
+    for(int i = 0; i < encrypted_text.size(); i += 8) {
+        std::cout << decrypted_cipher_text.substr(i, 8) << std::endl;
+        decrypted_bytes.push_back(decrypted_cipher_text.substr(i, 8));
+    }
+
+    return decrypted_bytes;
+}
+
+int main(int argc, char** argv) {
+    std::string mode = "none";
     std::string in_file, out_file;
 
     argh::parser cmdl(argv);
 
     cmdl({"-f", "--file"}) >> in_file;
     cmdl({"-o", "--output"}) >> out_file;
+
     if(cmdl[{"-v", "--version"}]) {
         std::cout << "ver1.0" << std::endl;
         exit(0);
     }
 
+    if (cmdl[{"-e", "--encrypt-mode"}]) {
+        mode = "encrypt";
+    }
+
+    if (cmdl[{"-d", "--decrypt-mode"}]) {
+        mode = "decrypt";
+    }
+
+
     if(cmdl[{"-h", "--help"}]) {
         std::cout << "-v, --version        Display program version info and exit." << std::endl;
         std::cout << "-f, --file TEXT      Takes a file as an input." << std::endl;
         std::cout << "-o, --output TEXT    Writes the output to the specified file." << std::endl;
+        std::cout << "-e, --encrypt-mode   Runs in encrypt mode." << std::endl;
+        std::cout << "-d, --decrypt-mode   Runs in decrypt mode." << std::endl;
         std::cout << "--help, -h           show help" << std::endl;
         exit(0);
     }
@@ -59,52 +81,79 @@ int main(int argc, char** argv) {
         exit(0);
     }
 
-    std::string key{"0111000101110111011001010111001001110100011110010111010101101001"}, plaintext;
+    std::string key{"0111000101110111011001010111001001110100011110010111010101101001"}, input_string;
 
-    std::vector<std::bitset<8>> bit_set_vec;
 
     std::ifstream fin;
     std::ofstream fout;
-    
-    std::tuple <std::string, std::string> res;
-    
+
+    std::tuple<std::string, std::string> result;
+
     fin.open(in_file);
     fout.open(out_file);
-    
+
     if(fin) {
-        // getline(fin, plaintext);
         std::ostringstream ss;
         ss << fin.rdbuf();
-        plaintext = ss.str();
+        input_string = ss.str();
     }
 
-    for(std::size_t i = 0; i < plaintext.size(); i++) {
-        bit_set_vec.push_back(std::bitset<8>(plaintext.c_str()[i]));
+    std::vector<std::bitset<8>> bit_set_vec;
+    for(std::size_t i = 0; i < input_string.size(); i++) {
+        bit_set_vec.push_back(std::bitset<8>(input_string.c_str()[i]));
     }
+
     if(bit_set_vec.size() % 8) {
         int padding = 8 - bit_set_vec.size() % 8;
         for (int i = 0; i < padding; i++) {
             bit_set_vec.push_back(std::bitset<8>());
         }
     }
-    std::string plaintext_buffered{""};
-    std::vector<std::string> encrypted_bytes;
-    for(int i = 0; i < bit_set_vec.size(); i++) {
-        plaintext_buffered = plaintext_buffered + bit_set_vec[i].to_string();
-        // std::cout << plaintext_buffered.size() << std::endl;
 
-        if(plaintext_buffered.size() == 64) {
-            std::cout << plaintext_buffered << "sent to cipher" << std::endl;
-            encrypted_bytes = cipher_decipher(key, plaintext_buffered);
-            for(int i = 0; i < encrypted_bytes.size(); i++) {
-                std::bitset<8> enc_char(encrypted_bytes[i]);
-                unsigned long x = enc_char.to_ulong();
-                unsigned char c =  static_cast<unsigned char>(x);
-                fout << c;
+    if (mode == "encrypt") {
+        std::string plaintext_buffered;
+        std::vector<std::string> encrypted_bytes;
+
+        for(int i = 0; i < bit_set_vec.size(); i++) {
+            plaintext_buffered = plaintext_buffered + bit_set_vec[i].to_string();
+
+            if(plaintext_buffered.size() == 64) {
+                std::cout << plaintext_buffered << "sent to cipher" << std::endl;
+                encrypted_bytes = encrypt(key, plaintext_buffered);
+                for(int j = 0; j < encrypted_bytes.size(); j++) {
+                    std::bitset<8> enc_char(encrypted_bytes[j]);
+                    unsigned long x = enc_char.to_ulong();
+                    unsigned char c =  static_cast<unsigned char>(x);
+                    fout << c;
+                }
+
+                plaintext_buffered = "";
             }
-            plaintext_buffered = "";
         }
+    } else if (mode == "decrypt") {
+        std::string encrypted_text_buffered;
+        std::vector<std::string> decrypted_bytes;
+
+        for(int i = 0; i < bit_set_vec.size(); i++) {
+            encrypted_text_buffered = encrypted_text_buffered + bit_set_vec[i].to_string();
+
+            if(encrypted_text_buffered.size() == 64) {
+                std::cout << encrypted_text_buffered << "sent to cipher" << std::endl;
+                decrypted_bytes = decrypt(key, encrypted_text_buffered);
+                for(int j = 0; j < decrypted_bytes.size(); j++) {
+                    std::bitset<8> enc_char(decrypted_bytes[j]);
+                    unsigned long x = enc_char.to_ulong();
+                    unsigned char c =  static_cast<unsigned char>(x);
+                    fout << c;
+                }
+
+                encrypted_text_buffered = "";
+            }
+        }
+    } else {
+        std::cout << "No mode selected. Checkout --help to get more info" << std::endl;
     }
+
     fout << std::endl;
 
     fout.close();
